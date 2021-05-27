5f24fdfa435cecdcf76c8a1ad055db9e2f78f1a6
for-alisia <for-alisia@yandex.ru>
2021-05-27 10:00:16 +0500

    Added colored button with the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // find an element with a role of button and text "Change to blue"
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  // expect the background color to be import { connect } from 'react-redux'
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect the button text to be changed 'Change to red"
+  expect(colorButton.textContent).toBe('Change to red');
 });
