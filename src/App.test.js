0f7655b938363c1307418bedaa7d4cf697b4b890
for-alisia <for-alisia@yandex.ru>
2021-05-28 10:05:38 +0500

    Added linter

--- src/App.test.js
+++ src/App.test.js
@@ -17,7 +17,7 @@ test('button has correct initial color', () => {
   expect(colorButton).toHaveStyle({ backgroundColor: 'MidnightBlue' });
 
   // expect the button text to be changed 'Change to red"
-  expect(colorButton.textContent).toBe('Change to Medium Violet Red');
+  expect(colorButton).toHaveTextContent('Change to Medium Violet Red');
 });
 
 test('initial conditions', () => {
