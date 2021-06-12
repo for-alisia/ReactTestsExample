7a80d10612489421627327c3c203aef78ded0170
for-alisia <for-alisia@yandex.ru>
2021-06-12 14:50:41 +0500

    Added tests for toppings subtotals

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
-const ToppingOption = ({ name, imagePath }) => {
+const ToppingOption = ({ name, imagePath, updateItemCount }) => {
+  const handleChange = (event) => {
+    const count = event.target.checked ? 1 : 0;
+    updateItemCount(name, count);
+  };
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img src={`http://localhost:3030/${imagePath}`} alt={`${name} topping`} />
+      <Form.Group controlId={name} as={Row} style={{ marginTop: '10px' }}>
+        <Form.Label column xs="6" style={{ textAlign: 'right' }}>
+          {name}
+        </Form.Label>
+        <Col xs="5" style={{ textAlign: 'left' }}>
+          <Form.Control type="checkbox" onChange={handleChange}></Form.Control>
+        </Col>
+      </Form.Group>
     </Col>
   );
 };
--- src/pages/entry/test/totalUpdates.test.jsx
+++ src/pages/entry/test/totalUpdates.test.jsx
@@ -9,7 +9,7 @@ test('update scoop subtotal when scoop changes', async () => {
   const scoopSubtotal = screen.getByText('Scoops total: $', { exact: false });
   expect(scoopSubtotal).toHaveTextContent('0.00');
 
-  // update vanilla scoops to 1 and ckack a subtotal
+  // update vanilla scoops to 1 and check a subtotal
   const vanillaInput = await screen.findByRole('spinbutton', { name: 'Vanilla' });
   userEvent.clear(vanillaInput);
   userEvent.type(vanillaInput, '1');
@@ -20,4 +20,29 @@ test('update scoop subtotal when scoop changes', async () => {
   userEvent.clear(chocolateInput);
   userEvent.type(chocolateInput, '2');
   expect(scoopSubtotal).toHaveTextContent('6.00');
+});
+
+test('update toppings subtotal when scoop changes', async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingsSubtotal = screen.getByText('Toppings total: $', { exact: false });
+
+  // find and choose Hot fuge
+  const hotFuge = await screen.findByRole('checkbox', { name: 'Hot fudge' });
+  userEvent.clear(hotFuge);
+  userEvent.click(hotFuge);
+
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
+
+  // find and choose Gummi bears
+  const gummiBears = await screen.findByRole('checkbox', { name: 'Gummi bears' });
+  userEvent.clear(gummiBears);
+  userEvent.click(gummiBears);
+
+  expect(toppingsSubtotal).toHaveTextContent('3.00');
+
+  // uncheck Gummi bears
+  userEvent.click(gummiBears);
+
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
 });
